8255095594c0220afe0a67741f487045d3033373
nondi58 <ntalukder190@gmail.com>
2024-11-24 22:59:01 +0600

    second commit

--- ch.js
+++ ch.js
@@ -1,7 +1,14 @@
-const value = [1, 2, 3];
 
-if (Array.isArray(value)) {
-    console.log("It's an array!");
-} else {
-    console.log("It's not an array.");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+   
--- first.js
+++ first.js
@@ -1,102 +1,120 @@
 
 function calculateTax(income, expenses) {
-    if(expenses>income ){
-        return "Invalid Input" ;
+    if (expenses > income) {
+        return "Invalid Input";
     }
-    else if(income<0){
-        return "Invalid Input" ;
+    else if (income < 0) {
+        return "Invalid Input";
 
     }
-    else if(expenses<0){
-        return "Invalid Input" ;
+    else if (expenses < 0) {
+        return "Invalid Input";
 
     }
-    else{
-        const r=income-expenses;
-        const reminder=r*0.20;
+    else {
+        const r = income - expenses;
+        const reminder = r * 0.20;
         return reminder;
     }
 }
 
 function checkDigitsInName(name) {
-   
- 
-    if(typeof(name)==='string'){
+
+
+    if (typeof (name) === 'string') {
         return 'false'
     }
-    else if(typeof(name)==='string'&&(isNaN(parseInt(name)) )){
+    else if (typeof (name) === 'string' && (isNaN(parseInt(name)))) {
         return 'true';
     }
-    else if(typeof(name)==='number'){
+    else if (typeof (name) === 'number') {
         return 'nub'
     }
- 
-    
+
+
 }
 
 
 function sendNotification(email) {
-   const chkIndex=email.indexOf('@');
-   if(chkIndex===-1){
-    return "Invalid Email"
-   }
-   else if(chkIndex===0  ){
-    return "Invalid Email"
+    const chkIndex = email.indexOf('@');
+    if (chkIndex === -1) {
+        return "Invalid Email"
+    }
+    else if (chkIndex === 0) {
+        return "Invalid Email"
 
-   }
-   else if(chkIndex===email.length-1){
-      return "Invalid Email"
+    }
+    else if (chkIndex === email.length - 1) {
+        return "Invalid Email"
 
-   }
-   else if(chkIndex===''){
-      return "Invalid Email"
+    }
+    else if (chkIndex === '') {
+        return "Invalid Email"
 
-   }
-   else{
-    const userInfo=  email.slice(0,chkIndex );
-    const domInfo=email.slice(chkIndex+1 );
-    const fullInformation=userInfo + " sent you an email from " +domInfo;
+    }
+    else {
+        const userInfo = email.slice(0, chkIndex);
+        const domInfo = email.slice(chkIndex + 1);
+        const fullInformation = userInfo + " sent you an email from " + domInfo;
 
-    return fullInformation;
-   }
+        return fullInformation;
+    }
 }
 
 function calculateFinalScore(obj) {
-    if(typeof(obj) !=="object"){
-        return  "Invalid Input";
-    }else{ 
-        let totalScore=obj.testScore+obj.schoolGrade;
-        if(obj.isFFamily){
-            totalScore=totalScore+20;
+    if (typeof (obj) !== "object") {
+        return "Invalid Input";
+    } else {
+        let totalScore = obj.testScore + obj.schoolGrade;
+        if (obj.isFFamily) {
+            totalScore = totalScore + 20;
 
         }
-      
-        if(totalScore<80){
 
-             return "false";
-        }else{
-            return"true";
-        }  
-           
+        if (totalScore < 80) {
+
+            return "false";
+        } else {
+            return "true";
+        }
+
     }
 }
 
 
 
 
-function  waitingTime(waitingTimes  , serialNumber) {
-    if (Array.isArray(waitingTimes) && typeof(serialNumber)==='number'){
-        let sum=0;
-        for(let i of waitingTimes){
+function waitingTime(waitingTimes, serialNumber) {
+    if (Array.isArray(waitingTimes) && typeof (serialNumber) === 'number') {
+        let sum = 0;
+        let avg = 0;
+        let totalAvg;
+        let curentSerial;
+let remainArray;
+let waitingTime;
+
+
+        for (let i = 0; i < waitingTimes.length; i++) {
+            sum = sum + waitingTimes[i];
+            avg = sum / waitingTimes.length;
+            totalAvg=(Math.round(avg));
           
+            if(serialNumber>waitingTimes[i]){
+                curentSerial=serialNumber-1;
+                remainArray=curentSerial-waitingTimes.length;
+                waitingTime=remainArray* totalAvg;
+               }
+
         }
-        return sum;
+        return  waitingTime;
 
     }
-    else{
-        return"Invalid Input";
+    else {
+        return "Invalid Input";
     }
 }
-console.log(waitingTime([ 3, 5, 7, 11, 6 ], 10));
+
+
+
 
 
